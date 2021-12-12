c85790f0bf9f5ddc4da6d8dba11f7f50ab52d4e8
L1ghtYagam1 <Yaska2608@mail.ru>
2021-12-12 23:01:57 +0300

    add 1

--- underlordapp/ViewController.swift
+++ underlordapp/ViewController.swift
@@ -9,9 +9,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var sliderRed: UISlider!
+    @IBOutlet weak var sliderGren: UISlider!
+    @IBOutlet weak var SliderBlue: UISlider!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var grenLable: UILabel!
+    @IBOutlet weak var blueLable: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        
+        colorView.layer.cornerRadius = 15
+               
+               sliderRed.minimumTrackTintColor = .red
+               sliderGren.minimumTrackTintColor = .green
+               
+               setColor()
+               setValue(for: redLabel, grenLable, blueLable)
+           }
+           
+           @IBAction func rgbSlider(_ sender: UISlider) {
+               setColor()
+               switch sender {
+               case sliderRed:
+                   redLabel.text = string(from: sliderRed)
+               case sliderGren:
+                   grenLable.text = string(from: sliderGren)
+               default:
+                   blueLable.text = string(from: SliderBlue)
+               }
+           }
+           
+           private func setColor() {
+               colorView.backgroundColor = UIColor(
+                   red: CGFloat(sliderRed.value),
+                   green: CGFloat(sliderGren.value),
+                   blue: CGFloat(SliderBlue.value),
+                   alpha: 1
+               )
+           }
+           
+           private func setValue(for labels: UILabel...) {
+               labels.forEach { label in
+                   switch label {
+                   case redLabel:
+                       redLabel.text = string(from: sliderRed)
+                   case grenLable:
+                       grenLable.text = string(from: sliderGren)
+                   default:
+                       blueLable.text = string(from: SliderBlue)
+                   }
+               }
+           }
+           
+           private func string(from slider: UISlider) -> String {
+               String(format: "%.2f", slider.value)
+
     }
 
 
